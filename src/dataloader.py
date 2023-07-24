65736c6da782e5ea1ceae73621f10986c2fc52a0
Ariadne Matos <46492977+aryamtos@users.noreply.github.com>
2023-07-24 09:56:22 -0300

    refactor: create function add_noise

--- src/dataloader.py
+++ src/dataloader.py
@@ -6,16 +6,24 @@ from torch.utils.data import Subset
 from sklearn.model_selection import KFold 
 
 class SpectrogramDataset(Dataset):
-    def __init__(self, filenames, labels, spec):
+    
+    def __init__(self, filenames, labels, spec,noise):
         self.filenames = filenames
         self.labels = labels
         self.spec = spec
         self.input_shape, self.preprocessing_fn= spec.waveform_spectrogram(filenames[0])
-
+        self.noise = noise
+       
     def convert_tensor(self, tensor):
         numpy_array = tensor.numpy()
         pytorch_tensor = torch.from_numpy(numpy_array)
         return pytorch_tensor
+
+    def add_noise(self,spectrog):
+        numpy_spectrog = spectrog.numpy()
+        noise = torch.randn_like(torch.from_numpy(numpy_spectrog)) * self.noise
+        noisy_spectrogram = spectrog + noise
+        return noisy_spectrogram
 
     def __len__(self):
         return len(self.filenames)
@@ -23,13 +31,13 @@ class SpectrogramDataset(Dataset):
     def __getitem__(self, idx):
         filename = self.filenames[idx]
         label = self.labels[idx]
+        #print(label)
         spectrogram = self.preprocessing_fn(filename)
+        spectrogram = self.add_noise(spectrogram)
         spectrogram = self.convert_tensor(spectrogram)
-        #spectrogram = spectrogram.transpose(0,1)  # altera a ordem das dimensões
+        #augmented_spectrogram = self.augmentation(spectrogram)
         label = torch.tensor(label, dtype=torch.long)
-        
         return spectrogram, label
-    
 
 class SplitAndComputeWeights():
 
@@ -63,9 +71,9 @@ class SplitAndComputeWeights():
         weights_val = self.compute_weights(val_ds)
         weighted_sampler_train =  torch.utils.data.WeightedRandomSampler(weights_train, len(train_ds), replacement=True)
         weighted_sampler_val = torch.utils.data.WeightedRandomSampler(weights_val, len(val_ds), replacement=True)
-        trainloader = torch.utils.data.DataLoader(train_ds, batch_size=32, sampler= weighted_sampler_train)
-        val_dataloader = torch.utils.data.DataLoader(val_ds,batch_size=32, sampler=weighted_sampler_val)
-        test_dataloader = torch.utils.data.DataLoader(test_ds,batch_size=32, num_workers=0)
+        trainloader = torch.utils.data.DataLoader(train_ds, batch_size=64, sampler= weighted_sampler_train)
+        val_dataloader = torch.utils.data.DataLoader(val_ds,batch_size=64, sampler=weighted_sampler_val)
+        test_dataloader = torch.utils.data.DataLoader(test_ds,batch_size=64, num_workers=0)
 
         return trainloader, val_dataloader,test_dataloader
 
