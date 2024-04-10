93e67e5e5b227fb88a057693e66f992c15d9bbe5
Ariadne <ariadne_nascimento@hotmail.com>
2024-04-10 00:35:34 -0300

    build: modified main.sh

--- cnn2d/main.sh
+++ cnn2d/main.sh
@@ -1,14 +1,15 @@
-DATASET_TRAIN="/store/amatos/pasta/projects/segmentation/src/train_spotify_spect/"
-DATASET_VAL="/store/amatos/pasta/projects/segmentation/src/dev_spotify_spect/"
-DATASET_TEST="/store/amatos/pasta/projects/segmentation/src/test_/"
+DATASET_TRAIN=""
+DATASET_VAL=""
+DATASET_TEST="/test_/"
 CHECKPOINT="checkpoint.pt"
 BATCH_SIZE=32
 LR=0.0001
 DECAY=2e-5
 N_EPOCHS=50
 PATIENCE=25
+SEED=42
 
-#python3 run.py train --dataset_train=$DATASET_TRAIN --dataset_val=$DATASET_VAL --batch_size=$BATCH_SIZE --lr=$LR  --decay=$DECAY --n_epochs=$N_EPOCHS --patience=$PATIENCE
+#python3 run.py train --dataset_train=$DATASET_TRAIN --dataset_val=$DATASET_VAL --batch_size=$BATCH_SIZE --lr=$LR  --decay=$DECAY --n_epochs=$N_EPOCHS --patience=$PATIENCE --seed=$SEED
 #python3 run.py eval --dataset_test=$DATASET_TEST --batch_size=$BATCH_SIZE --checkpoint=$CHECKPOINT
 
 if [ -f "$CHECKPOINT" ]; then
